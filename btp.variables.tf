28e89bc0994ff7b06624e7dfa30168f1788b0380
E970884_ibe <E970884+ibe@iberdrola.com>
2025-10-29 11:35:19 +0100

    Update btp.variables.tf

--- btp.variables.tf
+++
@@ -1,9 +0,0 @@
-variable "globalaccount" {
-  type        = string
-  description = "The subdomain of the global account in which you want to manage resources. To be found in the cockpit, in the global account view"
-  validation {
-    condition     = var.globalaccount != null && var.globalaccount != "needsToBePassedAsAnInputVariable"
-    error_message = "Provide a valid value foralue for the Global Account [Input Variable]"
-  }
-}
-
